0720af18ae7bc37488196972ef40d1304c44ce67
asdf <romanfholes@gmail.com>
2024-12-04 16:05:34 +0100

    Mocky - Akoby Peto povedal skoro restApi

--- src/app/http-client-service.service.ts
+++ src/app/http-client-service.service.ts
@@ -1,50 +1,28 @@
 import { Injectable } from '@angular/core';
-import { InsideBook } from './inside-book';
 import { HttpClient } from '@angular/common/http';
-import { delay, Observable } from 'rxjs';
+import { Observable } from 'rxjs';
+import { InsideBook } from './inside-book';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TheLibraryService {
-  private apiUrl = 'localhost:4200';
-  constructor(private http:HttpClient) { }
-  getData():Observable<InsideBook[] | null>{
-    //treba dorobit return
-  
-    
-  }
-  BookComponentList: InsideBook[] = [
-    {
-      "id": 0,
-      "name": "FightClub",
-      "photo": "https://cdn11.bigcommerce.com/s-gibnfyxosi/images/stencil/1280x1280/products/111890/113654/513PNeKaKrL__02114.1615558356.jpg?c=1",
-      "nameAuthor": "we dont talk about fight club",
-      "obsah": "first rule of fight club, never talk about fight club",
-      "dostupnost": true,
-      "dostupnostmnozstvo": 3
-    },
-    {
-      "id": 1,
-      "name": "About Mice and People",
-      "photo": "https://th.bing.com/th/id/OIP.-8KH0RbN08qbOLuJbveIWQHaIp?rs=1&pid=ImgDetMain",
-      "nameAuthor": "John Steibeck",
-      "obsah": "friend kill a stupid friend gg",
-      "dostupnost": true,
-      "dostupnostmnozstvo": 1
-    },
-    {
-      "id": 2,
-      "name": "Neuromancer",
-      "photo": "https://th.bing.com/th/id/OIP.eMV8yFKBLX2YVesCR7MZcQHaLT?rs=1&pid=ImgDetMain",
-      "nameAuthor": "William Gibson",
-      "obsah": "I steal your soul",
-      "dostupnost": false,
-      "dostupnostmnozstvo": 0
-    }
-  ];
 
- 
+  // Use your Mocky URL here
+  private apiUrl = 'https://run.mocky.io/v3/2c749c80-4095-4744-860d-324b5113cdc4'; 
+
+  constructor(private http: HttpClient) { }
+
+  // Method to get books
+  getBooks(): Observable<{ insideBook: InsideBook[] }> {
+    return this.http.get<{ insideBook: InsideBook[] }>(this.apiUrl);
+  }
+
+  // Method to post book
+  postBook(book: InsideBook): Observable<InsideBook> {
+    return this.http.post<InsideBook>(this.apiUrl, book);
+  }
 }
+
 
   
--- src/app/library/library.component.ts
+++ src/app/library/library.component.ts
@@ -1,75 +1,72 @@
-import { Component, inject } from '@angular/core';
-import { BookComponent } from '../book/book.component';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InsideBook } from '../inside-book';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { TheLibraryService } from '../the-library.service';
-import { HttpClientModule } from '@angular/common/http';
+import { BookComponent } from '../book/book.component';
+
 @Component({
   selector: 'app-library',
   standalone: true,
-  imports: [CommonModule,BookComponent, FormsModule,HttpClientModule],
+  imports: [CommonModule, BookComponent, FormsModule],
   templateUrl: './library.component.html',
-  styleUrl: './library.component.css'
+  styleUrls: ['./library.component.css']
 })
+export class LibraryComponent implements OnInit {
+  state: number = 1;
+  bookComponentList: InsideBook[] = [];
+  idnumber = 3;
 
-
-
-
-export class LibraryComponent {
-  constructor(private router: Router, private libraryService: TheLibraryService)
-   { this.bookComponentList = this.libraryService.BookComponentList;
-    }
-
-state:Number = 1;
-
-bookComponentList: InsideBook[] = [];
-ngOnInit():void{
-this.libraryService.getBooks().subscribe((books:InsideBook[] | null)=>{
-  if(books != null){
-    this.state = 2;
-  }else if (books == null){
-    this.state = 3;
-  }
-});
-}
-
-
-   idnumber = 3;
-
-   //new book properties
-   newBook = {
+  newBook: InsideBook = {
     id: this.idnumber,
     name: '',
     photo: '',
     nameAuthor: '',
     obsah: '',
     dostupnost: false,
-    dostupnostmnozstvo: 0,
+    dostupnostmnozstvo: 0
   };
-  
 
-  //add book
-  addBook() {
-    if(this.newBook.dostupnostmnozstvo > 0){
-      this.newBook.dostupnost=true;
-    }else{
-      this.newBook.dostupnost=false;
-    }
-      console.log('New book:', this.newBook);
-      this.bookComponentList.push(this.newBook)
-      this.idnumber = this.idnumber +1;
-      this.newBook = {
-        id: this.idnumber ,
-        name: '',
-        photo: '',
-        nameAuthor: '',
-        obsah: '',
-        dostupnost: false,
-        dostupnostmnozstvo: 0,
+  constructor(private router: Router, private libraryService: TheLibraryService) {
+    this.bookComponentList = this.libraryService.BookComponentList;
+  }
+
+  ngOnInit(): void {
+    this.libraryService.getBooks().subscribe({
+      next: (response) => {
+        if (response) {
+          this.bookComponentList = response;
+          this.bookComponentList = response;
+          this.state = 2;
+        } else {
+          this.state = 3;
+        }
+      },
+      error: (error) => {
+        console.error('Error fetching books:', error);
+        this.state = 3;
       }
-          
-    
+    });
+  }
+
+  addBook(): void {
+    if (this.newBook.dostupnostmnozstvo > 0) {
+      this.newBook.dostupnost = true;
+    } else {
+      this.newBook.dostupnost = false;
+    }
+    console.log('New book:', this.newBook);
+    this.bookComponentList.push(this.newBook);
+    this.idnumber = this.idnumber + 1;
+    this.newBook = {
+      id: this.idnumber,
+      name: '',
+      photo: '',
+      nameAuthor: '',
+      obsah: '',
+      dostupnost: false,
+      dostupnostmnozstvo: 0
+    };
   }
 }
